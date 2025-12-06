f464e59ccc9554615cb6d158a667226b88640280
soykolibri <clarice.robenalt@gmail.com>
2025-12-06 14:52:56 -0800

    Staging ICD-10-CM codes

---
+++ models/staging/stg_icd_10_cm_codes.sql
@@ -0,0 +1,20 @@
+{{ config(alias='icd_10_cm_codes') }}
+
+-- scrape all diagnosis codes from medical claims
+with unioned as (
+    {%- set diagnosis_cols = range(1, 25) -%}
+    {%- for n in diagnosis_cols %}
+        select diagnosis_code_{{ n }} as icd_10_cm_code
+        from {{ ref('medical_claim') }}
+        where
+            coalesce(diagnosis_code_type, '') = 'icd-10-cm'
+            and diagnosis_code_{{ n }} is not null
+        {%- if not loop.last %} union all {% endif -%}
+    {%- endfor %}
+)
+-- dedupe codes and classify which are cancer-related
+select 
+    distinct icd_10_cm_code,
+    {{ enrich_cancer_code(icd_10_cm_code, 'anatomical_site') }} as cancer_anatomical_site,
+    {{ enrich_cancer_code(icd_10_cm_code, 'severity') }} as cancer_severity
+from unioned
